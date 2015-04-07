c8686ee07f32b2974724f0edeccdf363c7b7ce7a
lpawlik91 <lukasz.pawlik@91.pl>
2015-04-07 21:54:57 +0200

    adding USB receiver sender stub
    USB receiver-sender stub for LPC1768

---
+++ lpc1768/USB_Receiver_Sender.c
@@ -0,0 +1,35 @@
+#include "LPC17xx.h"
+#include "lpc17xx_clkpwr.h"
+#include "lpc_types.h"
+
+
+#include "usb.h"
+#include "usbcfg.h"
+#include "usbhw.h"
+#include "usbcore.h"
+#include "cdc.h"
+#include "cdcuser.h"
+#include "serial.h"
+
+#define MAX_TEMP_BUFFOR 32
+
+void USB_Receiver_Sender()
+{
+	static char serBuf [USB_CDC_BUFSIZE];
+	int  numBytesToRead, numBytesRead, numAvailByte;
+
+	USB_Init();
+	USB_Connect(TRUE);                        // USB Connect
+	while (!USB_Configuration) ;              // wait until USB is configured
+	while(1)
+	{
+		CDC_OutBufAvailChar (&numAvailByte);
+		if (numAvailByte > 0)
+		{
+		  	numBytesToRead = numAvailByte > MAX_TEMP_BUFFOR ? MAX_TEMP_BUFFOR : numAvailByte;		//get max 32 bytes
+		    numBytesRead = CDC_RdOutBuf (&serBuf[0], &numBytesToRead);
+		    USB_WriteEP (CDC_DEP_IN, (unsigned char *)&serBuf[0], numBytesRead);
+		}
+	}
+
+}
---
+++ lpc1768/main.c
@@ -0,0 +1,11 @@
+void USB_Receiver_Sender();
+
+int main(void)
+{
+
+	USB_Receiver_Sender();
+	while(1)
+    {
+    }
+}
+
