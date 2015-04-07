d11857b066b71e6ad68bbdef4b593c2b54654cc3
lpawlik91 <lukasz.pawlik@91.pl>
2015-04-07 21:10:27 +0200

    little fix

--- libusbtest2/libusbtest2/libusbtest2.cpp
+++ libusbtest2/libusbtest2/libusbtest2.cpp
@@ -3,36 +3,16 @@
 
 #include "stdafx.h"
 #include "libusb.h"
+#include <cstdlib>
+#include <cstdio>
 #include <iostream>
 
 using namespace std;
 
-void printdev(libusb_device *dev); //prototype of the function
-
 int _tmain(int argc, _TCHAR* argv[])
 {
-	/*libusb_device **devs; //pointer to pointer of device, used to retrieve a list of devices
-	libusb_context *ctx = NULL; //a libusb session
-	int r; //for return values
-	ssize_t cnt; //holding number of devices in list
-	r = libusb_init(&ctx); //initialize a library session
-	if(r < 0) {
-		cout<<"Init Error "<<r<<endl; //there was an error
-				return 1;
-	}
-	libusb_set_debug(ctx, 3); //set verbosity level to 3, as suggested in the documentation
-	cnt = libusb_get_device_list(ctx, &devs); //get the list of devices
-	if(cnt < 0) {
-		cout<<"Get Device Error"<<endl; //there was an error
-	}
-	cout<<cnt<<" Devices in list."<<endl; //print total number of usb devices
-		ssize_t i; //for iterating through the list
-	for(i = 0; i < cnt; i++) {
-				printdev(devs[i]); //print specs of this device
-		}
-		libusb_free_device_list(devs, 1); //free the list, unref the devices in it
-		libusb_exit(ctx); //close the session
-		*/
+
+
 	libusb_device **devs; //pointer to pointer of device, used to retrieve a list of devices
 	libusb_device_handle *dev_handle; //a device handle
 	libusb_context *ctx = NULL; //a libusb session
@@ -50,23 +30,19 @@ int _tmain(int argc, _TCHAR* argv[])
 		cout<<"Get Device Error"<<endl; //there was an error
 		return 1;
 	}
-	cout<<cnt<<" Devices in list."<<endl;
-/*	ssize_t i; //for iterating through the list
-	for(i = 0; i < cnt; i++) {
-				printdev(devs[i]); //print specs of this device
-		}
-	int d;
-	cin >> d;
-	return 0; */
-	dev_handle = libusb_open_device_with_vid_pid(ctx, 0x125F, 0xA15A); //these are vendorID and productID I found for my usb device
+
+
+	dev_handle = libusb_open_device_with_vid_pid(ctx, 0x1fc9, 0x2002); //0x125f, 0xa15a); //these are vendorID and productID I found for my usb device
 	if(dev_handle == NULL)
 		cout<<"Cannot open device"<<endl;
 	else
 		cout<<"Device Opened"<<endl;
+
+	
 	libusb_free_device_list(devs, 1); //free the list, unref the devices in it
 
-	unsigned char *data = new unsigned char[4]; //data to write
-	data[0]='a';data[1]='b';data[2]='c';data[3]='d'; //some dummy values
+	unsigned char *data = new unsigned char[5]; //data to write
+	data[0]='a';data[1]='b';data[2]='c';data[3]='d';data[4]='\0'; //some dummy values 
 
 	int actual; //used to find out how many bytes were written
 	if(libusb_kernel_driver_active(dev_handle, 0) == 1) { //find out if kernel driver is attached
@@ -74,7 +50,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		if(libusb_detach_kernel_driver(dev_handle, 0) == 0) //detach it
 			cout<<"Kernel Driver Detached!"<<endl;
 	}
-	r = libusb_claim_interface(dev_handle, 0); //claim interface 0 (the first) of device (mine had jsut 1)
+	r = libusb_claim_interface(dev_handle, 1);//0); //claim interface 0 (the first) of device (mine had jsut 1)
 	if(r < 0) {
 		cout<<"Cannot Claim Interface"<<endl;
 		return 1;
@@ -83,13 +59,26 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 	cout<<"Data->"<<data<<"<-"<<endl; //just to see the data we want to write : abcd
 	cout<<"Writing Data..."<<endl;
-	r = libusb_bulk_transfer(dev_handle, (2 | LIBUSB_ENDPOINT_OUT), data, 4, &actual, 0); //my device's out endpoint was 2, found with trial- the device had 2 endpoints: 2 and 129
-	if(r == 0 && actual == 4) //we wrote the 4 bytes successfully
+	//r = libusb_bulk_transfer(dev_handle, (129 | LIBUSB_ENDPOINT_OUT), data, 4, &actual, 0); //my device's out endpoint was 2, found with trial- the device had 2 endpoints: 2 and 129
+	r = libusb_bulk_transfer(dev_handle, (2 | LIBUSB_ENDPOINT_OUT), data, 5, &actual, 0); //my device's out endpoint was 2, found with trial- the device had 2 endpoints: 2 and 129
+	if(r == 0 && actual == 5) //we wrote the 4 bytes successfully
 		cout<<"Writing Successful!"<<endl;
 	else
 		cout<<"Write Error"<<endl;
+
+
+	unsigned char data_r[5];
+	int actual_length;
+	int r2 = libusb_bulk_transfer(dev_handle, (2 | LIBUSB_ENDPOINT_IN), data_r, sizeof(data), &actual_length, 0);
+	if (r2 == 0 && actual_length == sizeof(data)) {
+		printf("\n\nRead: %s\n", data_r);
+
+	} else {
+		printf("\n\nError Read: %c");
+		//error();
+	}
 	
-	r = libusb_release_interface(dev_handle, 0); //release the claimed interface
+	r = libusb_release_interface(dev_handle, 1); //0); //release the claimed interface
 	if(r!=0) {
 		cout<<"Cannot Release Interface"<<endl;
 		return 1;
@@ -106,6 +95,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
+
+/*
 
 void printdev(libusb_device *dev) 
 {
@@ -151,3 +142,5 @@ void printdev(libusb_device *dev)
 	libusb_free_config_descriptor(config);
 
 }
+
+*/
