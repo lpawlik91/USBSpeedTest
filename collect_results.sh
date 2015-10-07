5f37b945877a2d1d5c3d7558c34b4179512ccf0a
Łukasz <lukasz.pawlik@91.pl>
2015-10-07 07:22:50 +0200

    more tests

--- collect_results.sh
+++ collect_results.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
 
-BUFFER=64
+BUFFER=20
 MODE=S
-SIZE_SET=1073742824
+SIZE_SET=10737420
 FILENAME="$MODE"
 FILENAME+="_"
 FILENAME+="$SIZE_SET"
 FILENAME+=".data"
+
 while [ $BUFFER -gt 1 ]; do
 	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
 	let BUFFER=BUFFER-2
 done
 
-BUFFER=64
+BUFFER=20
 MODE=A
-SIZE_SET=1073742824
-FILENAME="$MODE"
-FILENAME+="_"
-FILENAME+="$SIZE_SET"
-FILENAME+=".data"
-while [ $BUFFER -gt 1 ]; do
-	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
-	let BUFFER=BUFFER-2
-done
-
-BUFFER=64
-MODE=S
-SIZE_SET=1073742
-FILENAME="$MODE"
-FILENAME+="_"
-FILENAME+="$SIZE_SET"
-FILENAME+=".data"
-
-while [ $BUFFER -gt 1 ]; do
-	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
-	let BUFFER=BUFFER-2
-done
-
-BUFFER=64
-MODE=A
-SIZE_SET=1073742
+SIZE_SET=10737420
 FILENAME="$MODE"
 FILENAME+="_"
 FILENAME+="$SIZE_SET"
