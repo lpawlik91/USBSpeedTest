6a1ff3e5fef6fdc1c8594f772c6d38e8fa0cddf1
Łukasz <lukasz.pawlik@91.pl>
2015-10-08 18:01:39 +0200

    more results

--- collect_results.sh
+++ collect_results.sh
@@ -1,79 +1,27 @@
 #!/bin/bash
 
-BUFFER=20
-MODE=S
-SIZE_SET=10737420
-FILENAME="$MODE"
-FILENAME+="_"
-FILENAME+="$SIZE_SET"
-FILENAME+=".data"
-
-while [ $BUFFER -gt 1 ]; do
-	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
-	let BUFFER=BUFFER-2
-done
-
-BUFFER=20
-MODE=A
-SIZE_SET=10737420
-FILENAME="$MODE"
-FILENAME+="_"
-FILENAME+="$SIZE_SET"
-FILENAME+=".data"
-
-while [ $BUFFER -gt 1 ]; do
-	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
-	let BUFFER=BUFFER-2
-done
-
 BUFFER=64
 MODE=S
-SIZE_SET=107374
+SIZE_SET=107374200
 FILENAME="$MODE"
 FILENAME+="_"
 FILENAME+="$SIZE_SET"
 FILENAME+=".data"
 
-while [ $BUFFER -gt 1 ]; do
+while [ $BUFFER -gt 21 ]; do
 	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
 	let BUFFER=BUFFER-2
 done
 
 BUFFER=64
 MODE=A
-SIZE_SET=107374
+SIZE_SET=107374200
 FILENAME="$MODE"
 FILENAME+="_"
 FILENAME+="$SIZE_SET"
 FILENAME+=".data"
 
-while [ $BUFFER -gt 1 ]; do
-	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
-	let BUFFER=BUFFER-2
-done
-
-BUFFER=64
-MODE=S
-SIZE_SET=10737
-FILENAME="$MODE"
-FILENAME+="_"
-FILENAME+="$SIZE_SET"
-FILENAME+=".data"
-
-while [ $BUFFER -gt 1 ]; do
-	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
-	let BUFFER=BUFFER-2
-done
-
-BUFFER=64
-MODE=A
-SIZE_SET=10737
-FILENAME="$MODE"
-FILENAME+="_"
-FILENAME+="$SIZE_SET"
-FILENAME+=".data"
-
-while [ $BUFFER -gt 1 ]; do
+while [ $BUFFER -gt 21 ]; do
 	./libusbtest2/libusbtest2/libusb $MODE $BUFFER $SIZE_SET 1 >> Results/$FILENAME
 	let BUFFER=BUFFER-2
 done
