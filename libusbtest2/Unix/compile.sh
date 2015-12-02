9b3f2be62ce6894d64855dfa6170295a23022ffd
Łukasz <lukasz.pawlik@91.pl>
2015-12-02 22:01:30 +0100

    compilation between two systems

---
+++ libusbtest2/Unix/compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm *.cpp *.hpp makefile
+cp ../libusbtest2/*.cpp ../libusbtest2/*.hpp ../libusbtest2/makefile .
+rm stdafx.cpp
+sed -i -- 's/#include "stdafx.h"//g' *.cpp
+sed -i -- 's/#include "stdafx.h"//g' *.hpp
+sed -i '1s/^/#include "Mode.hpp"/' Mode.cpp
+sed -i '1s/^/#include "SynchMode.hpp"/' SynchMode.cpp
+sed -i '1s/^/#include "AsynchMode.hpp"/' AsynchMode.cpp
+
+sed -i -- 's/#pragma once/#pragma once\n#include "Mode.hpp"\n/g' SynchMode.hpp
+sed -i -- 's/#pragma once/#pragma obce\n#include "Mode.hpp"\n/g' AsynchMode.hpp
+#sed -i '1s/^/#include "Mode.hpp"\n/' SynchMode.hpp
+#sed -i '1s/^/#include "Mode.hpp"\n/' AsynchMode.hpp
+make clean
+make
