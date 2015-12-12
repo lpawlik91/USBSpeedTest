1ea07535155116eb0077b678c8b4133e9547686b
Łukasz <lukasz.pawlik@91.pl>
2015-12-13 00:51:23 +0100

    ututut

--- libusbtest2/Unix/UT/libusb_gt.cpp
+++ libusbtest2/Unix/UT/libusb_gt.cpp
@@ -1,11 +1,32 @@
-#include <limits.h>
+#include <memory>
+#include "SynchMode.hpp"
 #include "gtest/gtest.h"
+#include <stdexcept>
+
+class ModeTest : public ::testing::Test 
+{ 
+public: 
+
+   void SetUp( ) 
+   { 
+       synchMode.reset(new SynchMode(128, 256, 0x123d, 0x5678, 0));
+   }
+
+   void TearDown( ) 
+   { 
+   }
+   
+private:
+   std::shared_ptr<Mode> synchMode;
+   
+};
 
 
-TEST(Test, first) 
+TEST_F(ModeTest, getContext) 
 {
 	int a = 1;
 	EXPECT_EQ(a, 1);
+//	EXPECT_THROW(synchMode->getcontext(), std::runtime_error("Init Context error"));
 }
 
 
